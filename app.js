99a5869c80deef315b3984e96ada452559d17446
Daniel <marin.daniel1608@gmail.com>
2020-04-03 22:13:50 -0600

    aa

--- app.js
+++ app.js
@@ -8,6 +8,18 @@ app.use(bodyParser.json());
 
 //import 
 
+const camionesWeb = require('./WebForms/Camiones');
+const bodegasWeb = require('./WebForms/Bodegas');
+const clientesWeb = require('./WebForms/Clientes');
+const consecutivosWeb = require('./WebForms/Consecutivos');
+const eventosWeb = require('./WebForms/Eventos');
+const materiaprimaWeb = require('./WebForms/Materia Prima');
+const procesosWeb = require('./WebForms/Procesos');
+const productosWeb = require('./WebForms/Productos');
+const proveedoresWeb = require('./WebForms/Proveedores');
+const tiposmateriaprimasWeb = require('./WebForms/Tipos de Materia Prima');
+const usuariosWeb = require('./WebForms/Usuarios');
+
 const postsRoute = require('./routes/posts');
 const rolesRoute = require('./routes/roles');
 const eventosRoute = require('./routes/eventos');
@@ -17,6 +29,18 @@ const bodegasRoute = require('./routes/bodegas');
 const camionesRoute = require('./routes/camiones');
 const tpmRoute = require('./routes/tipos_materia_prima');
 const mpRoute = require('./routes/materia_prima');
+
+app.use('/posts', camionesWeb);
+app.use('/posts', bodegasWeb);
+app.use('/posts', clientesWeb);
+app.use('/posts', consecutivosWeb);
+app.use('/posts', eventosWeb);
+app.use('/posts', materiaprimaWeb);
+app.use('/posts', procesosWeb);
+app.use('/posts', productosWeb);
+app.use('/posts', proveedoresWeb);
+app.use('/posts', tiposmateriaprimasWeb);
+app.use('/posts', usuariosWeb);
 
 app.use('/posts', postsRoute);
 app.use('/roles', rolesRoute);
